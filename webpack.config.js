ea8b6603344c087d5b1d07a089d71d2c33a93437
Bryan Kahler <bkahler3@gmail.com>
2016-12-23 23:40:36 -0800

    takingout some old config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-dconst path = require('path')
+const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
@@ -59,41 +59,3 @@ module: {
     }
   ]
 }
-
-
-// const webpack = require('webpack');
-
-// module.exports = {
-//   entry: [
-//     './src/index.js'
-//   ],
-//   output: {
-//     path: __dirname,
-//     publicPath: '/',
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     loaders: [{
-//       exclude: /node_modules/,
-//       loader: 'babel',
-//       query: {
-//         presets: ['react', 'es2015', 'stage-1']
-//       }
-//     }]
-//   },
-//   //This section allows react to access env variables
-//   plugins:[
-//      new webpack.DefinePlugin({
-//       'process.env':{
-//         'YOUTUBEAPI': JSON.stringify(process.env.YOUTUBEAPI)
-//       }
-//     })
-//   ],
-//   resolve: {
-//     extensions: ['', '.js', '.jsx']
-//   },
-//   devServer: {
-//     historyApiFallback: true,
-//     contentBase: './'
-//   }
-// };
